db510f4090db925a2eb23aad5108281aab37482a
ferington <155619800+ferington@users.noreply.github.com>
2024-03-08 18:05:19 +0900

    [start]

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+
+  scope module: :user do
+    root to: 'homes#top'
+    get "about" => "homes#about", as:"about"
+    
+    
+  end
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
+
+  devise_for :customers, controllers: {
+    registrations: "user/registrations",
+    sessions: 'user/sessions'
+  }
+
+
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+end
