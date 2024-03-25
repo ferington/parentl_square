f86ae2356a496ab016ee48cf794c580be54bff34
ferington <155619800+ferington@users.noreply.github.com>
2024-03-25 17:00:09 +0900

    [fix]タグ表示修正

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,10 @@ class Tag < ApplicationRecord
   has_many :posts, through: :post_tags
 
   validates :name, presence:true, length:{maximum:50}
+
+  # アクティブな投稿に紐づいくタグのみを返すスコープ
+  def self.active
+    Tag.joins(:posts).where(posts: {is_deleted: false}).distinct
+  end
+
 end
